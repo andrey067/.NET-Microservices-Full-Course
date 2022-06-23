2ba501345144dfc7d713d2645313e480b36872c4
Audrey  Ernesto de Lima <3970@ucdb.br>
2022-06-23 17:17:17 -0400

    Finalizando platformservice

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,22 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+PrepDb.PrepPopulation(builder.Services, builder.Environment.IsProduction());
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
